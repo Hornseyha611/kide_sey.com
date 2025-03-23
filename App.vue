2fd3424b38d4356437f156324d949f37a03d577e
Horn Seyha <hounsey190@gmail.com>
2025-03-23 23:05:50 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+    
+  
+<Header />
+ 
+<section_body />
+<footer_app />
+
+ </template>
+<script setup>
+
+import footer_app from'./components/footer_app.vue'
+import Header from'./components/Header.vue';
+import section_body from './components/section_body.vue';
+
+ 
+ 
+
+
+</script>
+<style scoped>
+ 
+
+</style>
